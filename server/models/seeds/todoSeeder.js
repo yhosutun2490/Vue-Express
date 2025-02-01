35730d1e9be2b0604b0ece6329e4a61e5d1a3e73
yhosutun2491 <yhosutun2491@gmail.com>
2025-02-01 20:40:19 +0800

    feat: add todo seeder

---
+++ server/models/seeds/todoSeeder.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Todo = require('../todos') // 載入 todo model
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 連接db
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  // db連接後的邏輯
+  for (let i = 0; i < 10; i++) {
+    Todo.create({name:`name-${i}`})
+  }
+  console.log('done')
+})
